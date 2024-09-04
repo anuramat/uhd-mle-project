76f9544b7a7c366186a29837be263306eee58e5c
Arsen Nuramatov <anuramat@pm.me>
2024-09-04 14:16:59 +0200

    feat(deps): add flake and direnv

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.python3.withPackages (python-pkgs:
+              with python-pkgs; [
+                # jupyter
+                # jupytext
+                numpy
+                scipy
+                scikit-learn
+                tqdm
+                pygame
+                torchWithCuda
+              ]))
+          ];
+        };
+      }
+    );
+}
