c90abca20845da73841862affef0eacfbe9c2843
Arsen Nuramatov <anuramat@pm.me>
2024-09-07 04:08:16 +0200

    feat(watcher): init dataset agent

---
+++ agent_code/watcher/callbacks.py
@@ -0,0 +1,9 @@
+import agent_code.rule_based_agent.callbacks as based
+
+
+def setup(self):
+    based.setup(self)
+
+
+def act(self, game_state: dict):
+    based.act(self, game_state)
---
+++ agent_code/watcher/train.py
@@ -0,0 +1,13 @@
+from agent_code.vkl.utils import get_map
+
+
+def setup_training(self):
+    pass
+
+
+def game_events_occurred(self, state, action, new_state, events):
+    pass
+
+
+def end_of_round(self, state, action, events):
+    pass
