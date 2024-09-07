fa1badc3eb1a4feedfc7d0e57e2d15bda9b2aae1
Arsen Nuramatov <anuramat@pm.me>
2024-09-07 05:23:47 +0200

    fix(vkl): more tests, still doesn't work

--- agent_code/vkl/test/test_utils.py
+++ agent_code/vkl/test/test_utils.py
@@ -1,12 +1,26 @@
 import torch
+from torch import tensor
 from agent_code.vkl.utils import get_pov
 
 
-def test_get_pov():
-    base = torch.ones(1, 3, 3)
-    print(base)
+map_base = torch.ones(1, 3, 3)
+map_random = torch.rand(1, 5, 5)
 
-    trivial = get_pov(base, (1, 1), 1)
-    print(trivial)
 
-    assert torch.equal(base, trivial)
+class TestGetPov:
+    def test_random_center(self):
+
+        random = get_pov(map_random, (2, 2))
+        random_expected = map_random[:, 1:4, 1:4]
+        print(random)
+        print(random_expected)
+        assert torch.equal(random, random_expected)
+
+    def test_trivial(self):
+        trivial = get_pov(map_random, (2, 2), 2)
+        assert torch.equal(trivial, map_random)
+
+    def test_corner(self):
+        corner = get_pov(map_base, (0, 0))
+        print(corner)
+        assert torch.equal(corner, map_base)  # TODO
