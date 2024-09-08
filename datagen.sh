164f3ae91c5681152c535aca296d9b1dd77116f3
Arsen Nuramatov <anuramat@pm.me>
2024-09-08 06:47:23 +0200

    fix(data): minor

--- datagen.sh
+++ datagen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # 400 moves * 4 agents = 1600 moves
-python main.py play --agents watcher watcher watcher watcher --train 4 --no-gui --n-rounds 10
+python main.py play --agents watcher watcher watcher watcher --train 4 --no-gui --n-rounds 100
