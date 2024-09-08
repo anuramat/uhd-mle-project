b294c6caee620e36b98fd30d84573e557945e9fd
Arsen Nuramatov <anuramat@pm.me>
2024-09-08 06:25:28 +0200

    fix(watcher): collect everything in one file

--- agent_code/watcher/train.py
+++ agent_code/watcher/train.py
@@ -1,28 +1,28 @@
 from agent_code.vkl.utils import get_map
 from collections import namedtuple
 from torch import save
-import random
 
 Move = namedtuple("Move", ["map", "player", "action"])
 
-copy_counter = 0
+
+class Saver:
+    def __init__(self, path):
+        self.moves = []
+        self.path = path
+
+    def __del__(self):
+        save(self.moves, self.path)
 
 
 def setup_training(self):
-    self.moves = []
-    self.rounds_played = 0
-    global copy_counter
-    self.number = copy_counter
-    copy_counter += 1
+    self.saver = Saver("moves.pt")
 
 
 def game_events_occurred(self, state, action, new_state, events):
-    move = Move(map=get_map(state), player=state["self"], action=action)
+    move = (get_map(state), state["self"], action)
     # TODO save action as a number, save only necessary fields of self
-    self.moves.append(move)
+    self.saver.moves.append(move)
 
 
 def end_of_round(self, state, action, events):
-    self.rounds_played += 1
-    save(self.moves, f"data/moves_{self.number}_{self.rounds_played}.pt")
-    self.moves = []
+    pass
--- datagen.sh
+++ datagen.sh
@@ -2,4 +2,4 @@
 
 # 100MB of data
 # 400 moves * 4 agents * 10 games = 16000 moves
-python main.py play --agents watcher watcher watcher watcher --train 4 --no-gui
+python main.py play --agents watcher watcher watcher watcher --train 4 --no-gui --n-rounds 10
