ebb78a87b6c839609ff2d67cd7ae817be01e65db
Arsen Nuramatov <anuramat@pm.me>
2024-09-08 10:09:14 +0200

    feat(vkl): basic cnn and tests

--- agent_code/vkl/model.py
+++ agent_code/vkl/model.py
@@ -1,1 +1,35 @@
-pass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from agent_code.vkl.utils import ACTIONS
+
+
+class BinaryImageClassifier(nn.Module):
+    def __init__(self, n_chan, radius):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(n_chan, 16, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+        )
+
+        offset = 4
+        self.conv_output_dim = 32 * (radius - offset) ** 2
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.conv_output_dim + 1, 128),
+            nn.ReLU(),
+            nn.Linear(128, len(ACTIONS)),
+        )
+
+    def forward(self, bombful, map):
+        x = self.conv(map)
+
+        x = x.reshape(-1, self.conv_output_dim)
+        x = torch.cat((x, bombful.reshape(-1, 1)), dim=1)
+
+        x = self.fc(x)
+
+        return F.softmax(x, dim=1)
---
+++ agent_code/vkl/test/test_model.py
@@ -0,0 +1,17 @@
+from torch import tensor, randn
+from agent_code.vkl.model import BinaryImageClassifier
+from agent_code.vkl.utils import STR2INT
+
+
+class TestModel:
+    def basic(self):
+        radius = 9
+        n_channels = 5
+
+        model = BinaryImageClassifier(n_channels, 9)
+
+        bombful = tensor([1])
+        map = randn(1, n_channels, radius, radius)
+
+        output = model(bombful, map)
+        assert list(output.shape) == [1, len(STR2INT)]
