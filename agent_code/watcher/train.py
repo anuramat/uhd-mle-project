032954d71c8f9ad524fc3da02d0e8b29d1be3079
Arsen Nuramatov <anuramat@pm.me>
2024-09-08 05:31:31 +0200

    feat(watcher): basic saving

--- agent_code/watcher/train.py
+++ agent_code/watcher/train.py
@@ -1,13 +1,22 @@
 from agent_code.vkl.utils import get_map
+from collections import namedtuple
+from torch import save
+
+Move = namedtuple("Move", ["map", "player", "action"])
 
 
 def setup_training(self):
-    pass
+    self.moves = []
+    self.rounds_played = 0
 
 
 def game_events_occurred(self, state, action, new_state, events):
-    pass
+    move = Move(map=get_map(state), player=state["self"], action=action)
+    # TODO save action as a number, save only necessary fields of self
+    self.moves.append(move)
 
 
 def end_of_round(self, state, action, events):
-    pass
+    self.rounds_played += 1
+    save(self.moves, f"moves_{self.rounds_played}.pt")
+    self.moves = []
