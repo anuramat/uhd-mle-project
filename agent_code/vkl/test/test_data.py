ba0bc78b24a58e6f2344c704b98b11f45169f715
Arsen Nuramatov <anuramat@pm.me>
2024-09-09 06:40:16 +0200

    fix(data): fix test

--- agent_code/vkl/test/test_data.py
+++ agent_code/vkl/test/test_data.py
@@ -1,4 +1,4 @@
-from agent_code.vkl.consts import N_CHANNELS
+from agent_code.vkl.consts import N_CHANNELS, ACTIONS
 from agent_code.vkl.data import MoveDataset
 from torch import load
 from torch.utils.data.dataloader import DataLoader
@@ -11,7 +11,7 @@ class TestDataset:
         input = load("agent_code/watcher/output/moves_0.pt")
         dataset = MoveDataset(input)
         dataloader = DataLoader(dataset, batch_size=batch_size)
-        map, action, bomb = next(iter(dataloader))
+        map, bomb, action = next(iter(dataloader))
         assert list(map.shape) == [batch_size, N_CHANNELS, 3, 3]
         assert list(bomb.shape) == [batch_size]
-        assert list(action.shape) == [batch_size]
+        assert list(action.shape) == [batch_size, len(ACTIONS)]
