ee8edabf93d7198918f53d4b57e8531fe8ec32ef
Arsen Nuramatov <anuramat@pm.me>
2024-09-09 06:41:05 +0200

    fix(model): fix test

--- agent_code/vkl/test/test_model.py
+++ agent_code/vkl/test/test_model.py
@@ -6,11 +6,12 @@ from agent_code.vkl.models import BasicModel
 class TestModels:
     def test_basic(self):
         radius = 9
+        side = radius * 2 + 1
 
         model = BasicModel(radius)
 
         bombful = tensor([1])
-        map = randn(1, N_CHANNELS, radius * 2 + 1, radius)
+        map = randn(1, N_CHANNELS, side, side)
 
         output = model(map, bombful)
         assert list(output.shape) == [1, len(ACTIONS)]
