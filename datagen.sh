b3d7ff4b140f3cd6f4cb57f280491af0ea96f36b
Arsen Nuramatov <anuramat@pm.me>
2024-09-09 12:12:17 +0200

    feat(datagen): minor

--- datagen.sh
+++ datagen.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # 400 moves per round
-export N_GAMES=1000
+[ -z "$1" ] && echo "provide number of rounds" && exit
+export N_GAMES=$1
 python main.py play --agents watcher watcher watcher watcher --train 4 --no-gui --n-rounds "$N_GAMES"
