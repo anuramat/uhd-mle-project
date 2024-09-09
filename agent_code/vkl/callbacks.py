e503549f3d93b48f0fe2d6827a1e511bec4f6410
Arsen Nuramatov <anuramat@pm.me>
2024-09-09 09:36:06 +0200

    feat(vkl): now it can actually play

--- agent_code/vkl/callbacks.py
+++ agent_code/vkl/callbacks.py
@@ -1,9 +1,22 @@
-from . import model, utils
+from torch import load, argmax, tensor
+from agent_code.vkl.consts import INT2STR
+from agent_code.vkl.utils import get_map, get_pov
 
 
 def setup(self):
-    pass
+    self.model, self.fov = load("output/model.pt")
 
 
 def act(self, game_state: dict):
-    pass
+    map = get_map(game_state)
+
+    player = game_state["self"]
+    bomb = player[2]
+    x, y = player[3]
+    pov = get_pov(map, (y, x), self.fov)
+
+    proba = self.model(pov.float(), tensor(bomb).float())
+    index = int(argmax(proba))
+    action = INT2STR[index]
+
+    return action
