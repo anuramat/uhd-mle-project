ef318a6c484253fc011f47d10052f61cebe5fd09
Arsen Nuramatov <anuramat@pm.me>
2024-09-09 04:44:57 +0200

    fix(models): fix test

--- agent_code/vkl/test/test_model.py
+++ agent_code/vkl/test/test_model.py
@@ -1,17 +1,16 @@
 from torch import tensor, randn
+from agent_code.vkl.consts import N_CHANNELS, ACTIONS
 from agent_code.vkl.models import BasicModel
-from agent_code.vkl.utils import ACTIONS
 
 
 class TestModels:
-    def basic(self):
+    def test_basic(self):
         radius = 9
-        n_channels = 5
 
-        model = BasicModel(n_channels, 9)
+        model = BasicModel(radius)
 
         bombful = tensor([1])
-        map = randn(1, n_channels, radius, radius)
+        map = randn(1, N_CHANNELS, radius, radius)
 
         output = model(bombful, map)
         assert list(output.shape) == [1, len(ACTIONS)]
