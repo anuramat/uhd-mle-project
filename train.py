2203e338cb160f35c9e4f8883502a2452137bd8c
Arsen Nuramatov <anuramat@pm.me>
2024-09-17 00:56:15 +0200

    feat(train): add argparse

--- train.py
+++ train.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python
 
+
+import argparse
 from agent_code.vkl.data import TranDataset
 from gc import collect
-from os import listdir
+from os import listdir, cpu_count
 from os.path import exists, join
 from re import search
 from torch import load, save
 from torch.utils.data import DataLoader
 import agent_code.vkl.models as models
 import pytorch_lightning as L
-import sys
 import torch
 
 # from pytorch_lightning.tuner.tuning import Tuner
 
-load_existing = True
-if len(sys.argv) > 1 and sys.argv[1] == "new":
-    load_existing = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n-epochs", type=int, required=True)
+parser.add_argument("--source-model", type=str)
+parser.add_argument("--n-workers", type=int, default=cpu_count())
+parser.add_argument("--batch-size", type=int, default=1024)
+args = parser.parse_args()
 
 # parameters
-epochs = 2000
-batch_size = 1024
 torch.set_float32_matmul_precision("medium")
 precision = "16-mixed"
-num_workers = 32
 source_model = "source_model.pt"
 result_model_path = "result_model.pt"
 trans_dir = "agent_code/watcher/output/"
@@ -35,9 +37,9 @@ trans = sum([load(path, weights_only=False) for path in paths], [])
 dataset = TranDataset(trans)
 dataloader = DataLoader(
     dataset,
-    batch_size=batch_size,
+    batch_size=args.batch_size,
     shuffle=True,
-    num_workers=num_workers,
+    num_workers=args.n_workers,
     pin_memory=True,
     persistent_workers=True,
 )
@@ -46,22 +48,22 @@ collect()
 
 # raw model
 raw_model = None
-if load_existing:
-    if not exists(source_model):
+if args.source_model:
+    if not exists(args.source_model):
         raise FileNotFoundError("Source model file doesn't exist")
     print("Loading existing model")
-    raw_model = load(source_model)
+    raw_model = load(args.source_model)
 else:
     print("Starting training from scratch")
     raw_model = models.MyBelovedCNN()
     # HACK workaround for the tuner bug with the lazy layers: initialize manually
     map, aux, _, _ = next(iter(dataloader))
 
-total_steps = epochs * len(dataloader)
+total_steps = args.n_epochs * len(dataloader)
 
 # lightning
 model = models.Lighter(raw_model, total_steps=total_steps)
-trainer = L.Trainer(accelerator="gpu", max_epochs=epochs, precision=precision)
+trainer = L.Trainer(accelerator="gpu", max_epochs=args.n_epochs, precision=precision)
 # # TODO figure out how to make tuner ignore the scheduler
 # tuner = Tuner(trainer)
 # tuner.lr_find(model, dataloader)
